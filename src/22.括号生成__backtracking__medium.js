cc106118eb8c39d05876742af37c26d00ab68ba0
zhangpaopao <zhangpaopao0609@gmail.com>
2022-11-02 20:05:01 +0800

    feat: 22

---
+++ src/22.括号生成__backtracking__medium.js
@@ -0,0 +1,16 @@
+/*
+ * @lc app=leetcode.cn id=22 lang=javascript
+ *
+ * [22] 括号生成
+ */
+
+// @lc code=start
+/**
+ * @param {number} n
+ * @return {string[]}
+ */
+const generateParenthesis = function(n) {
+
+};
+// @lc code=end
+
